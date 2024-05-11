853c8cfb168124ae8075af78188fe6d53b81fe0d
eunsu <songess@naver.com>
2024-05-11 21:46:26 +0900

    refactor: map 리팩토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-'use client'
+'use client';
 import Script from 'next/script';
-import { useRef } from 'react';
-type NaverMap = naver.maps.Map;
-
-
-const mapId = 'map';
 
 export default function Home() {
   const initializeMap = () => {
     const mapOptions = {
-      center: new window.naver.maps.LatLng(37.3595704, 127.105399),
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
       zoom: 15,
-      scaleControl: true,
-      mapDataControl: true,
     };
-    const map = new window.naver.maps.Map(mapId, mapOptions);
+    const map = new naver.maps.Map('map', mapOptions);
   };
   return (
     <main className="flex w-full h-full flex-col items-center justify-center">
@@ -23,7 +16,7 @@ export default function Home() {
         src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}`}
         onLoad={initializeMap}
       />
-       <div id = {mapId} className='w-full h-full'/>
+      <div id="map" className="w-full h-full" />
     </main>
   );
 }
