3a8f6e961ea44af6187103b110b591a18d845ac9
eunsu <songess@naver.com>
2024-05-11 21:11:57 +0900

    feat: naver map 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+'use client'
+import Script from 'next/script';
+import { useRef } from 'react';
+type NaverMap = naver.maps.Map;
+
+
+const mapId = 'map';
 
 export default function Home() {
+  const initializeMap = () => {
+    const mapOptions = {
+      center: new window.naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 15,
+      scaleControl: true,
+      mapDataControl: true,
+    };
+    const map = new window.naver.maps.Map(mapId, mapOptions);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hello world</div>
+    <main className="flex w-full h-full flex-col items-center justify-center">
+      <Script
+        type="text/javascript"
+        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}`}
+        onLoad={initializeMap}
+      />
+       <div id = {mapId} className='w-full h-full'/>
     </main>
   );
 }
